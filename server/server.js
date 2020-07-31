a81baaeab036d9e7f406dac7c97d765935961ed6
Levi <levicarlson123@gmail.com>
2020-07-31 15:02:47 -0500

    setting up serverside files
    Co-authored-by: Jared Lidenberg <jaredl101@hotmail.com>
    Co-authored-by: hunterEdward98 <hunter.scheel@outlook.com>

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require("dotenv").config();
+const bodyParser = require('body-parser');
+
+const sessionMiddleware = require("./modules/session-middleware");
+
+const app = express();
+const passport = require("./strategies/user.strategy");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('server/public'));
+
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+const port = process.env.PORT || 5432;
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
