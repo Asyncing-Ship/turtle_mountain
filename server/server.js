0c3187e764d4c8f7b362c4ed6842687af7a15921
Hunter Scheel <hunter.scheel@outlook.com>
2020-07-31 22:00:33 -0500

    auto format and a comment

--- server/server.js
+++ server/server.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const sessionMiddleware = require("./modules/session-middleware");
 
 const app = express();
-
+//commenting for jared
 const passport = require("./strategies/user.strategy");
-require('./strategies/google.strategy');
-require('./strategies/facebook.strategy');
+require("./strategies/google.strategy");
+require("./strategies/facebook.strategy");
 
-const userRouter = require('./routes/user.router');
-const oauthRouter = require('./routes/oauth.router');
+const userRouter = require("./routes/user.router");
+const oauthRouter = require("./routes/oauth.router");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('server/public'));
-
+app.use(express.static("server/public"));
 
 // Passport Session Configuration //
 app.use(sessionMiddleware);
@@ -25,10 +24,10 @@ app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/api/user', userRouter)
-app.use('/api/auth', oauthRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", oauthRouter);
 
 const port = process.env.PORT || 5432;
 app.listen(port, () => {
-  console.log('listening on port', port);
+  console.log("listening on port", port);
 });
