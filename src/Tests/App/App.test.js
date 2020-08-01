01ae0b9df6cb750bb66c60f69245974a424d97c9
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-01 17:04:35 -0500

    1 more test added

--- src/Tests/App/App.test.js
+++ src/Tests/App/App.test.js
@@ -2,9 +2,8 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import { App } from "../../Components/App/App";
 import toJson from "enzyme-to-json";
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import rootReducer from "../../Redux/Reducers/reducers";
-import { Provider } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { Button } from "@chakra-ui/core";
 const store = createStore(rootReducer);
@@ -34,4 +33,9 @@ it("renders Account header", () => {
     </nav>
   );
   expect(wrapper.contains(header)).toEqual(true);
+});
+it("renders without crashing", () => {
+  const mockColor = "David";
+  const wrapper = shallow(<App color={mockColor} />);
+  expect(toJson(wrapper)).toMatchSnapshot();
 });
