42d36d7889f9c65cf11f686b849299053e6ebfa4
Jake Schaffer <jakecr8guru@gmail.com>
2020-08-03 10:55:45 -0500

    added signin page component

---
+++ src/Components/Pages/SignIn/SignIn.jsx
@@ -0,0 +1,14 @@
+// ----- Start of imports -----
+// React Import:
+import React from "react";
+// ----- End of imports -----
+
+const SignIn = () => {
+  return (
+    <div>
+      <h1>This is the Sign In Page.</h1>
+    </div>
+  )
+}
+
+export default SignIn;
