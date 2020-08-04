e809fabbf8380cfef1df69c510054c25409d9c0c
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-03 20:01:53 -0500

    updated tasks page and tests

--- src/Tests/ViewsTask/ViewsTask.test.js
+++ src/Tests/ViewsTask/ViewsTask.test.js
@@ -3,10 +3,11 @@ import { shallow } from "enzyme";
 import AcceptTask from "../../Components/Pages/ViewsTask/AcceptTask";
 import CompleteTask from "../../Components/Pages/ViewsTask/CompleteTask";
 import NewTask from "../../Components/Pages/ViewsTask/NewTask";
-import TaskPage from "../../Components/Pages/ViewsTask/TaskPage";
+import { TaskPage } from "../../Components/Pages/ViewsTask/TaskPage";
 import toJson from "enzyme-to-json";
 import { createStore } from "redux";
 import rootReducer from "../../Redux/Reducers/reducers";
+import { Button, ButtonText } from "@chakra-ui/core";
 const store = createStore(rootReducer);
 it("renders correctly", () => {
   shallow(<AcceptTask />);
@@ -20,4 +21,30 @@ it("renders correctly", () => {
 });
 it("renders correctly", () => {
   shallow(<TaskPage />);
+});
+it("renders correctly", () => {
+  const wrapper = shallow(<TaskPage />);
+  const header = <div>No Tasks to Display</div>;
+  expect(wrapper.contains(header)).toEqual(true);
+});
+it("renders correctly", () => {
+  const wrapper = shallow(
+    <TaskPage
+      tasks={[
+        { id: 1, title: "Task 1", status: "open" },
+        { id: 2, title: "Task 2", status: "taken" },
+      ]}
+    />
+  );
+  const header = (
+    <>
+      <Button>
+        Task 1<div className="btn-text">open</div>
+      </Button>
+      <Button>
+        Task 2<div className="btn-text">taken</div>
+      </Button>
+    </>
+  );
+  expect(wrapper.contains(header)).toEqual(true);
 });
