0ae2407ce85648361bb12b847758562f54256351
Levi <levicarlson123@gmail.com>
2020-08-04 11:12:27 -0500

    fixed login/signup styling

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,10 +1,3 @@
-/* the only line you likely need to change is
-
- database: 'prime_app',
-
- change `prime_app` to the name of your database, and you should be all set!
-*/
-
 const pg = require("pg");
 const url = require("url");
 
@@ -17,19 +10,17 @@ if (process.env.DATABASE_URL) {
   const auth = params.auth.split(":");
 
   config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    user: auth[0],
+    password: auth[1],
     host: params.hostname,
     port: params.port,
     database: params.pathname.split("/")[1],
     ssl: { rejectUnauthorized: false },
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  }; // postgresql://[user[:password]@][netloc][:port][/turtle_mountain_connect_develop]
+  };
 } else {
   config = {
-    user: "postgres",
-    password: "postgres",
     host: "localhost", // Server hosting the postgres database
     port: 5432, // env var: PGPORT
     database: "turtle_mountain_connect_development", // Database name
