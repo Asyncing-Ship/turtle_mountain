79d972e3c6fbb226d1d613fe1cf9ffc274d6361a
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-04 12:01:05 -0500

    fixed app test

--- src/Tests/App/App.test.js
+++ src/Tests/App/App.test.js
@@ -11,7 +11,7 @@ const store = createStore(rootReducer);
 it("renders correctly", () => {
   mount(
     <Provider store={store}>
-      <App />
+      <App user={{ id: 1 }} />
     </Provider>
   );
 });
