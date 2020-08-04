dc649850cadcbf3bf87b348e0d0b4bfd47eaa998
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-03 20:57:21 -0500

    Task Pages Somewhat done

--- src/Components/Pages/ViewsTask/TaskPage.js
+++ src/Components/Pages/ViewsTask/TaskPage.js
@@ -1,24 +1,52 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button, ButtonText } from "@chakra-ui/core";
+import { Button } from "@chakra-ui/core";
+import {
+  withRouter,
+  HashRouter as Router,
+  Link,
+  Route,
+} from "react-router-dom";
+import NewTask from "./NewTask";
+import AcceptTask from "./AcceptTask";
 export class TaskPage extends Component {
   componentDidMount() {
     //this.props.dispatch({ type: "FETCH_TASKS" });
   }
   render() {
     return (
-      <>
-        {this.props.tasks ? (
+      <Router>
+        <Route exact path="/tasks/newTask">
+          <NewTask />
+        </Route>
+        {this.props.tasks &&
           this.props.tasks.map((x) => (
-            <Button key={x.id}>
-              {x.title}
-              <div className="btn-text">{x.status}</div>
-            </Button>
-          ))
-        ) : (
-          <div>No Tasks to Display</div>
-        )}
-      </>
+            <Route exact path={`tasks/acceptTask/${x.id}`}>
+              <AcceptTask task={x} />
+            </Route>
+          ))}
+        <Route exact path="/tasks">
+          <Link to="/tasks/newTask">
+            <Button>New Task</Button>
+          </Link>
+          <br />
+          {this.props.tasks ? (
+            this.props.tasks.map((x) => (
+              <Button
+                onClick={() => {
+                  this.props.history.push(`/tasks/acceptTask/${x.id}`);
+                }}
+                key={x.id}
+              >
+                {x.title}
+                <div className="btn-text">{x.status}</div>
+              </Button>
+            ))
+          ) : (
+            <div>No Tasks to Display</div>
+          )}
+        </Route>
+      </Router>
     );
   }
 }
@@ -28,4 +56,4 @@ const mapStateToProps = (state) => {
     user: state.user,
   };
 };
-export default connect(mapStateToProps)(TaskPage);
+export default withRouter(connect(mapStateToProps)(TaskPage));
