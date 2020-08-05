39afc96d94c372f8002bdba216eb6cb75b6e13b7
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-05 13:34:53 -0500

    changed question files a bit. they're ready to receive props with maybe minor changes

--- src/Components/Pages/ViewsQuestion/AnswerQuestion.js
+++ src/Components/Pages/ViewsQuestion/AnswerQuestion.js
@@ -2,8 +2,20 @@ import React, { Component } from "react";
 import { Input } from "@chakra-ui/core";
 
 class AnswerQuestion extends Component {
+  state = { answer: "" };
+  handleChange = (event, value) => {
+    this.setState({
+      [value]: event.target.value,
+    });
+  };
   render() {
-    return <Input placeholder="Your answer"></Input>;
+    return (
+      <Input
+        value={this.state.answer}
+        placeholder="Your answer"
+        onChange={(event) => this.handleChange(event, "answer")}
+      ></Input>
+    );
   }
 }
 
