c39f1d2b3b83e77256722ed66c5f5d35060ff8cc
HerschelW <herschelw@gmail.com>
2020-08-04 20:04:06 -0500

    more task seeds added

--- server/seeders/20200804191442-demo-task.js
+++ server/seeders/20200804191442-demo-task.js
@@ -8,6 +8,21 @@ module.exports = {
         content: "of a task",
         added_by: 1,
       },
+      {
+        title: "This is another example",
+        content: "of a task",
+        added_by: 1,
+      },
+      {
+        title: "And another example",
+        content: "of a task",
+        added_by: 1,
+      },
+      {
+        title: "Last example",
+        content: "of a task",
+        added_by: 1,
+      },
     ]);
   },
   down: (queryInterface, Sequelize) => {
