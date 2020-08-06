823616f57da2c1977304239f39bf0e31f439b788
HerschelW <herschelw@gmail.com>
2020-08-06 12:05:33 -0500

    questions saga and reducer. task reducer typo fix

--- src/Redux/Sagas/index.js
+++ src/Redux/Sagas/index.js
@@ -3,6 +3,7 @@ import loginSaga from "./loginSaga";
 import registrationSaga from "./registrationSaga";
 import userSaga from "./userSaga";
 import tasksSaga from "./tasksSaga";
+import questionsSaga from "./questionsSaga";
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -12,5 +13,11 @@ import tasksSaga from "./tasksSaga";
 // the registration triggers a login
 // and login triggers setting the user
 export default function* rootSaga() {
-  yield all([userSaga(), loginSaga(), registrationSaga(), tasksSaga()]);
+  yield all([
+    userSaga(),
+    loginSaga(),
+    registrationSaga(),
+    tasksSaga(),
+    questionsSaga(),
+  ]);
 }
