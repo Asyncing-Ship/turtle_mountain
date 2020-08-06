d67d4c230a66fc4631d9c11268a44faacda8012c
Levi <levicarlson123@gmail.com>
2020-08-06 15:49:32 -0500

    updated pool config with env var for db name

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -22,7 +22,7 @@ if (process.env.DATABASE_URL) {
   config = {
     host: "localhost", // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-    database: "turtle_mountain_connect_development", // Database name
+    database: process.env.DB_NAME, // Database name
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
