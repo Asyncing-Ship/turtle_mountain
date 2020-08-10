aad454ff480218abdbef843c3f13da6fa4d41614
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-10 11:29:54 -0500

    question page tries to submit

--- src/Components/Pages/ViewsQuestion/Response.js
+++ src/Components/Pages/ViewsQuestion/Response.js
@@ -3,6 +3,6 @@ import { Input, Button } from "@chakra-ui/core";
 import { Textarea } from "@chakra-ui/core";
 import { connect } from "react-redux";
 const NewQuestion = (props) => {
-  return <div>{props.content}</div>;
+  return <div>{props.question.content}</div>;
 };
 export default connect()(NewQuestion);
