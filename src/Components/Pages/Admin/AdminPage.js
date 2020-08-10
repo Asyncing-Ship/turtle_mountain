05d14af066149c0a67b659951d922fdb6c0d7f99
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-10 14:32:35 -0500

    Admin Page ready for deploy, sir

--- src/Components/Pages/Admin/AdminPage.js
+++ src/Components/Pages/Admin/AdminPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Grid } from "@chakra-ui/core";
+import { Grid, Button } from "@chakra-ui/core";
 import { connect } from "react-redux";
 import Incoming from "./Incoming";
+import Approved from "./Approved";
 
 class AdminPage extends Component {
   state = {
@@ -13,6 +14,20 @@ class AdminPage extends Component {
   render() {
     return (
       <div>
+        <Button
+          onClick={() => {
+            this.setState({ sortBy: "incoming" });
+          }}
+        >
+          User Requests
+        </Button>
+        <Button
+          onClick={() => {
+            this.setState({ sortBy: "approved" });
+          }}
+        >
+          Approved Users
+        </Button>
         This is the admin page!
         {this.state.sortBy === "incoming" && (
           <Grid templateColumns="repeat(1, 1fr)" gap={3}>
@@ -23,6 +38,15 @@ class AdminPage extends Component {
               ))}
           </Grid>
         )}
+        {this.state.sortBy === "approved" && (
+          <Grid templateColumns="repeat(1, 1fr)" gap={3}>
+            {this.props.users
+              .filter((x) => x.isApproved)
+              .map((x) => (
+                <Approved user={x} />
+              ))}
+          </Grid>
+        )}
       </div>
     );
   }
@@ -30,7 +54,14 @@ class AdminPage extends Component {
 const mapStateToProps = (state) => {
   return {
     users: state.user.users || [
-      { first_name: "James", last_name: "Charles", id: 3 },
+      {
+        first_name: "Carrie",
+        last_name: "Underwood",
+        id: 2,
+        isApproved: true,
+        isAdmin: true,
+      },
+      { first_name: "James", last_name: "Charles", id: 3, isApproved: true },
       { first_name: "Felix", last_name: "Kjellburg", id: 4 },
     ],
   };
--- src/Components/Pages/Admin/Approved.js
+++ src/Components/Pages/Admin/Approved.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Box, Button, Icon } from "@chakra-ui/core";
+import { connect } from "react-redux";
 
-class Approved extends Component {
-  render() {
-    return (
-      <>
-        These people are approved:
-      </>
-    )
-  }
-}
+const Approved = (props) => {
+  return (
+    <Box>
+      <Box as="span" verticalAlign="top" ml={3}>
+        {props.user.first_name} {props.user.last_name}
+      </Box>
+      {!props.user.isAdmin && (
+        <Box as="span" verticalAlign="top" ml={3}>
+          <Button
+            onClick={() => {
+              props.dispatch({
+                type: "PROMOTE_USER",
+                payload: props.user.id,
+              });
+            }}
+          >
+            <Icon name="edit" size="16px" />
+          </Button>
+        </Box>
+      )}
+      <Box as="span" verticalAlign="top" ml={3}>
+        <Button
+          onClick={() => {
+            props.dispatch({
+              type: "DELETE_USER",
+              payload: props.user.id,
+            });
+          }}
+        >
+          <Icon name="close" size="16px" />
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
-export default Approved;
+export default connect()(Approved);
