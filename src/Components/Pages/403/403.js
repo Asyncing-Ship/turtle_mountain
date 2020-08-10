e3f99c05e57cabecad990ec18f636401ca12b0bf
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-10 16:11:41 -0500

    foreOhThree Done

--- src/Components/Pages/403/403.js
+++ src/Components/Pages/403/403.js
@@ -2,35 +2,8 @@ import React from "react";
 import "./403.css";
 const ForeOhThree = (props) => {
   return (
-    <div>
-      <div className="ghost">
-        <div className="ghost--navbar" />
-        <div className="ghost--columns">
-          <div className="ghost--column">
-            <div className="code" />
-            <div className="code" />
-            <div className="code" />
-            <div className="code" />
-          </div>
-          <div className="ghost--column">
-            <div className="code" />
-            <div className="code" />
-            <div className="code" />
-            <div className="code" />
-          </div>
-          <div className="ghost--column">
-            <div className="code" />
-            <div className="code" />
-            <div className="code" />
-            <div className="code" />
-          </div>
-        </div>
-        <div className="ghost--main">
-          <div className="code" />
-          <div className="code" />
-        </div>
-      </div>
-      <h1 className="police-tape police-tape--1">
+    <div className="page">
+      <h1 className="page police-tape police-tape--1">
         &nbsp;&nbsp;&nbsp;&nbsp;Error:
         403&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error:
         403&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error:
@@ -39,7 +12,7 @@ const ForeOhThree = (props) => {
         403&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error:
         403&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error: 403
       </h1>
-      <h1 className="police-tape police-tape--2">
+      <h1 className=" page police-tape police-tape--2">
         Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       </h1>
     </div>
