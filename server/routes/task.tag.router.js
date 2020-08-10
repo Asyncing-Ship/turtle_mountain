77a4a9df7ea3de1babd2d9857112de26bbb60539
Jared Lidenberg <jaredl101@hotmail.com>
2020-08-10 18:55:30 -0500

    Added task tagging router and fixed typos in question tag router.

--- server/routes/task.tag.router.js
+++ server/routes/task.tag.router.js
@@ -1,63 +1,63 @@
 const express = require("express");
-const Question = require("../models/question.model");
-const Question_Tag = require("../models/question_tag.model");
+const Task = require("../models/task.model");
+const Task_Tag = require("../models/task_tag.model");
 const User = require("../models/user.model");
 
 const router = express.Router();
 
 // This route *should* return the logged in users pets
 router.get("/", (req, res) => {
-  console.log("GET all question tags");
-  Question_Tag.findAll({
+  console.log("GET all task tags");
+  Task_Tag.findAll({
     include: [{ model: User }],
-    include: [{ model: Question }],
+    include: [{ model: Task }],
   })
     .then((responses) => {
-      // responses will be an array of all Question_Tag instances
+      // responses will be an array of all Task_Tag instances
       // console.log('Found all tags', responses);
       res.send(responses);
     })
     .catch((error) => {
-      console.log("Error getting all question tags", error);
+      console.log("Error getting all tasks tags", error);
       res.sendStatus(500);
     });
 });
 
 router.get("/:id", (req, res) => {
   let tagId = req.params.id;
-  console.log(`GET request for question tags with id  ${tagId}`);
-  Question_Tag.findAll({
+  console.log(`GET request for tasks tags with id  ${tagId}`);
+  Task_Tag.findAll({
     where: { id: tagId },
     include: [{ model: User }],
-    include: [{ model: Question }],
+    include: [{ model: Task }],
   })
     .then((responses) => {
-      console.log("Found question tags", responses);
+      console.log("Found task tags", responses);
       res.send(responses[0] || []);
     })
     .catch((error) => {
-      console.log(`Error getting question tags with id ${tagId}`, error);
+      console.log(`Error getting task tags with id ${tagId}`, error);
       res.sendStatus(500);
     });
 });
 
 router.post("/", (req, res) => {
   const userId = req.body.user.id;
-  const questionId = req.body.question_id;
-  console.log(`POST question tags, adding tags`, req.body);
+  const taskId = req.body.task_id;
+  console.log(`POST task tags, adding tags`, req.body);
 
-  let newQuestionTag = Question_Tag.build({
+  let newTaskTag = Task_Tag.build({
     userId: userId,
-    questionId: questionId,
+    taskId: taskId,
   });
   // Save to database
-  newQuestionTag
+  newTaskTag
     .save()
     .then((responses) => {
       res.sendStatus(200);
     })
     .catch((error) => {
-      console.log("Error adding question tags ", error);
+      console.log("Error adding task tags ", error);
       res.sendStatus(500);
     });
 });
@@ -65,13 +65,13 @@ router.post("/", (req, res) => {
 // Route to delete a tag
 router.delete("/:id", (req, res) => {
   let tagId = req.params.id;
-  console.log(`DELETE request for question tag with id ${tagId}`, req.body);
-  Question_Tag.destroy({ where: { id: tagId } })
+  console.log(`DELETE request for task tag with id ${tagId}`, req.body);
+  Task_Tag.destroy({ where: { id: tagId } })
     .then((responses) => {
       res.sendStatus(200);
     })
     .catch((error) => {
-      console.log(`Error deleting question tags with id ${tagId}`, error);
+      console.log(`Error deleting task tags with id ${tagId}`, error);
       res.sendStatus(500);
     });
 });
