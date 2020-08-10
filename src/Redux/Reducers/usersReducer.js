651ce322fab48a3a44fe29e0d7abeb5f87183624
Jared Lidenberg <jaredl101@hotmail.com>
2020-08-10 14:52:52 -0500

    Created users reducer, updated user saga with new actions, updated user router to allow approving and promoting.

---
+++ src/Redux/Reducers/usersReducer.js
@@ -0,0 +1,12 @@
+const usersReducer = (state = [], action) => {
+  switch (action.type) {
+    case "SET_ALL_USERS":
+      return action.payload;
+    case "UNSET_ALL_USERS":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
