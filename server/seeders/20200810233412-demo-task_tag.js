d916cd65f02ec038a9aa593274e5153ba512e47e
Jared Lidenberg <jaredl101@hotmail.com>
2020-08-12 18:24:33 -0500

    Updates to fix task tagging and seeds.

--- server/seeders/20200810233412-demo-task_tag.js
+++ server/seeders/20200810233412-demo-task_tag.js
@@ -6,14 +6,22 @@ module.exports = {
       {
         user_id: 1,
         task_id: 1,
+        tagged_users: [1, 2],
       },
       {
         user_id: 2,
-        task_id: 1,
+        task_id: 2,
+        tagged_users: [1, 2],
       },
       {
         user_id: 1,
-        task_id: 2,
+        task_id: 3,
+        tagged_users: [1, 2],
+      },
+      {
+        user_id: 1,
+        task_id: 4,
+        tagged_users: [1, 2],
       },
     ]);
   },
