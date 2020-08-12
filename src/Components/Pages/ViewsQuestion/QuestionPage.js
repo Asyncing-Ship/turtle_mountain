df92a567ea3bf9723fd9aef021f0c2c85e7974c3
Jake Schaffer <jakecr8guru@gmail.com>
2020-08-12 17:59:45 -0500

    finished styling

--- src/Components/Pages/ViewsQuestion/QuestionPage.js
+++ src/Components/Pages/ViewsQuestion/QuestionPage.js
@@ -15,7 +15,9 @@ import {
 // Chakra-ui imports:
 import {
   Input,
-  Button, Stack, ButtonGroup,
+  Button,
+  Stack,
+  ButtonGroup,
   Accordion,
   AccordionHeader,
   AccordionPanel,
@@ -23,10 +25,10 @@ import {
 } from "@chakra-ui/core";
 // Components Imports:
 import ProtectedRoute from "../../Utilities/ProtectedRoute/ProtectedRoute";
-import RecentQuestions from "./RecentQuestions"
-import UnansweredQuestions from "./UnansweredQuestions"
-import SearchQuestions from "./SearchQuestions"
-import NewQuestion from "./NewQuestion"
+import RecentQuestions from "./RecentQuestions";
+import UnansweredQuestions from "./UnansweredQuestions";
+import SearchQuestions from "./SearchQuestions";
+import NewQuestion from "./NewQuestion";
 import AnswerQuestion from "./AnswerQuestion";
 import Response from "./Response";
 // ----- End of imports -----
@@ -47,221 +49,78 @@ class QuestionPage extends Component {
   };
   render() {
     return (
-      <>
-        <RouterC>
-          <Stack className="tasks-content">
-            <ButtonGroup className="tasks-btngrp">
-              <NavLink
-                activeClassName="tasks-nav-active"
-                to="/recent-questions"
+      <RouterC>
+        <Stack className="tasks-content">
+          <ButtonGroup className="tasks-btngrp">
+            <NavLink activeClassName="tasks-nav-active" to="/recent-questions">
+              <Button
+                variant="outline"
+                variantColor="blue"
+                rightIcon="info"
+                m={3}
               >
-                <Button
-                  variant="outline"
-                  variantColor="blue"
-                  rightIcon="info"
-                  m={3}
-                >
-                  Most Recent
-                </Button>
-              </NavLink>
-              <NavLink
-                activeClassName="tasks-nav-active"
-                to="/unanswered-questions"
+                Most Recent
+              </Button>
+            </NavLink>
+            <NavLink
+              activeClassName="tasks-nav-active"
+              to="/unanswered-questions"
+            >
+              <Button
+                variant="outline"
+                variantColor="yellow"
+                rightIcon="star"
+                m={3}
               >
-                <Button
-                  variant="outline"
-                  variantColor="yellow"
-                  rightIcon="star"
-                  m={3}
-                >
-                  Unanswered
-                </Button>
-              </NavLink>
-              <NavLink
-                activeClassName="tasks-nav-active"
-                to="/search-questions"
+                Unanswered
+              </Button>
+            </NavLink>
+            <NavLink activeClassName="tasks-nav-active" to="/search-questions">
+              <Button
+                variant="outline"
+                variantColor="purple"
+                rightIcon="search"
+                m={3}
               >
-                <Button
-                  variant="outline"
-                  variantColor="purple"
-                  rightIcon="search"
-                  m={3}
-                >
-                  Search
-                </Button>
-              </NavLink>
-              <NavLink activeClassName="tasks-nav-active" to="/new-question">
-                <Button
-                  variant="outline"
-                  variantColor="green"
-                  rightIcon="add"
-                  m={3}
-                >
-                  New Question
-                </Button>
-              </NavLink>
-            </ButtonGroup>
-            <Redirect from="/" to="/recent-questions" />
-            <Switch>
-              <ProtectedRoute
-                exact
-                path="/recent-questions"
-                component={RecentQuestions}
-              />
-              <ProtectedRoute
-                exact
-                path="/unanswered-questions"
-                component={UnansweredQuestions}
-              />
-              <ProtectedRoute
-                exact
-                path="/search-questions"
-                component={SearchQuestions}
-              />
-              <ProtectedRoute
-                exact
-                path="/new-question"
-                component={NewQuestion}
-              />
-            </Switch>
-          </Stack>
-        </RouterC>
-        {/* --------------------------------------------------------------------------------- */}
-        {/* Start of buttons */}
-        <div>
-          {/* Start of buttons */}
-          <Button
-            m={3}
-            onClick={() => {
-              this.setState({
-                filter: 1,
-                searchText: "",
-              });
-            }}
-          >
-            Most Recent
-          </Button>
-          <Button
-            m={3}
-            onClick={() => {
-              this.setState({
-                filter: 2,
-                searchText: "",
-              });
-            }}
-          >
-            Unanswered
-          </Button>
-          {/* End of buttons */}
-          {/* Start of search input */}
-          <Input
-            m={3}
-            placeholder="Search For a Question"
-            onClick={() => {
-              this.setState({
-                filter: 3,
-              });
-            }}
-            onChange={(event) => {
-              this.setState({ searchText: event.target.value });
-            }}
-          />
-          {/* End of search input */}
-          {/* Start of Accordions Examples */}
-          {/* Start of Question area 1 */}
-          {this.state.filter === 1 && (
-            <Accordion defaultIndex={[-1]} allowToggle>
-              {this.props.questions.map((x) => (
-                <AccordionItem defaultIsOpen="False">
-                  <AccordionHeader
-                    className="accordionHead"
-                    onClick={() => this.setQuestion(x.id)}
-                  >
-                    {x.title}
-                  </AccordionHeader>
-                  <AccordionPanel>
-                    {x.content}
-                    <br />
-                    <br />
-                    <h3>Responses</h3>
-                    <br />
-                    {this.props.response[0] &&
-                      this.props.response.map((x) => <Response question={x} />)}
-                    <AnswerQuestion question={x} />
-                  </AccordionPanel>
-                </AccordionItem>
-              ))}
-            </Accordion>
-          )}
-          {/* End of Question area 1 */}
-          {/* Start of Question area 2 */}
-          {this.state.filter === 2 && (
-            <Accordion defaultIndex={[-1]} allowToggle>
-              {this.props.questions
-                .filter((x) => !x.is_answered)
-                .map((x) => (
-                  <AccordionItem defaultIsOpen="False">
-                    <AccordionHeader
-                      className="accordionHead"
-                      onClick={() => this.setQuestion(x.id)}
-                    >
-                      {x.title}
-                    </AccordionHeader>
-                    <AccordionPanel>
-                      {x.content}
-                      <br />
-                      <br />
-                      <h3>Responses</h3>
-                      <br />
-                      {this.props.response.map((x) => (
-                        <Response question={x} />
-                      ))}
-                      <AnswerQuestion question={x} />
-                    </AccordionPanel>
-                  </AccordionItem>
-                ))}
-            </Accordion>
-          )}
-          {/* End of Question area 2 */}
-          {/* Start of Question area 3 */}
-          {this.state.filter === 3 && (
-            <Accordion defaultIndex={[-1]} allowToggle>
-              {this.props.questions
-                .filter(
-                  (x) =>
-                    x.content.includes(this.state.searchText) ||
-                    x.title.includes(this.state.searchText)
-                )
-                .map((x) => (
-                  <AccordionItem defaultIsOpen="False">
-                    <AccordionHeader
-                      className="accordionHead"
-                      onClick={() => this.setQuestion(x.id)}
-                    >
-                      {x.title}
-                    </AccordionHeader>
-                    <AccordionPanel>
-                      {x.content}
-                      <br />
-                      <br />
-                      <h3>Responses</h3>
-                      <br />
-                      {this.props.response.map((x) => (
-                        <Response question={x} />
-                      ))}
-                      <AnswerQuestion />
-                    </AccordionPanel>
-                  </AccordionItem>
-                ))}
-            </Accordion>
-          )}
-          {/* End of Question area 3 */}
-          {/* Start of Accordions */}
-          <Link to="/questions/new">
-            <Button>New Question</Button>
-          </Link>
-        </div>
-      </>
+                Search
+              </Button>
+            </NavLink>
+            <NavLink activeClassName="tasks-nav-active" to="/new-question">
+              <Button
+                variant="outline"
+                variantColor="green"
+                rightIcon="add"
+                m={3}
+              >
+                New Question
+              </Button>
+            </NavLink>
+          </ButtonGroup>
+          <Redirect from="/" to="/recent-questions" />
+          <Switch>
+            <ProtectedRoute
+              exact
+              path="/recent-questions"
+              component={RecentQuestions}
+            />
+            <ProtectedRoute
+              exact
+              path="/unanswered-questions"
+              component={UnansweredQuestions}
+            />
+            <ProtectedRoute
+              exact
+              path="/search-questions"
+              component={SearchQuestions}
+            />
+            <ProtectedRoute
+              exact
+              path="/new-question"
+              component={NewQuestion}
+            />
+          </Switch>
+        </Stack>
+      </RouterC>
     );
   }
 }
