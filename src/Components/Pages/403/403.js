ea7446816e50a0761bf6437f7b58ee4a7963dff0
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-13 13:32:26 -0500

    403 message

--- src/Components/Pages/403/403.js
+++ src/Components/Pages/403/403.js
@@ -3,6 +3,12 @@ import "./403.css";
 const ForeOhThree = (props) => {
   return (
     <div className="page-body">
+      <h3>403! Oh, No!</h3>
+      <h4>
+        You're either getting this error because your account is waiting for
+        approval or
+      </h4>
+      you're on a page you shouldn't be on
       <div className="page">
         <h1 className="page police-tape police-tape--1">
           &nbsp;&nbsp;&nbsp;&nbsp;Error:
@@ -12,12 +18,12 @@ const ForeOhThree = (props) => {
           403&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error:
           403&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error:
           403&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Error: 403
-      </h1>
+        </h1>
         <h1 className=" page police-tape police-tape--2">
           Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      </h1>
+        </h1>
       </div>
     </div>
-  )
-}
+  );
+};
 export default ForeOhThree;
