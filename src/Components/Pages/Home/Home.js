3854a06798e9cad97a031bbba2d59f71bea1d8f1
Jake Schaffer <jakecr8guru@gmail.com>
2020-08-14 14:41:51 -0500

    buttons!

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,56 +1,161 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
-import { Box, Button, SimpleGrid } from "@chakra-ui/core";
+import { Box, Button, SimpleGrid, Text, Flex } from "@chakra-ui/core";
 import { Icon } from "@chakra-ui/core";
-import "./Home.css";
+// import "./Home.css";
 
 class Home extends Component {
   render() {
     const { history } = this.props;
     return (
       <Box>
-        <Button
-          m={2}
-          className="CircleBtn"
-          onClick={() => history.push("/tasks")}
-        >
-          <Icon name="warning" size="3.5rem" color="red.500" />
-          <div className="btn-text">tasks</div>
-        </Button>
+        {/* Start of Tasks Button */}
+        <Flex justify="center" align="center">
+          <Button
+            m={30}
+            height="160px"
+            width="160px"
+            rounded="50%"
+            variant="solid"
+            variantColor="tmarBlack"
+            bg="tmarBlack.800"
+            onClick={() => history.push("/tasks")}
+          >
+            <Flex
+              justifyContent="center"
+              alignContent="center"
+              direction="column"
+            >
+              <Box mt={1}>
+                <Icon name="warning" size="3.5rem" color="red.500" />
+              </Box>
+              <Box>
+                <Text as="h4" color="white" textTransform="uppercase" mt={4}>
+                  tasks
+                </Text>
+              </Box>
+            </Flex>
+          </Button>
+        </Flex>
+        {/* End of Tasks Button */}
         <SimpleGrid columns={[1, null, 3]}>
-          <Button
-            m={2}
-            className="CircleBtn"
-            onClick={() => history.push("/policies")}
-          >
-            <Icon name="info" size="3.5rem" color="blue.200" />
-            <div className="btn-text">policies</div>
-          </Button>
-          <Button
-            m={2}
-            className="CircleBtn"
-            onClick={() => history.push("/settings")}
-          >
-            <Icon name="settings" size="3.5rem" color="green.300" />
-            <div className="btn-text">settings</div>
-          </Button>
-          <Button
-            m={2}
-            className="CircleBtn"
-            onClick={() => history.push("/questions")}
-          >
-            <Icon name="question" size="3.5rem" color="yellow.300" />
-            <div className="btn-text">questions</div>
-          </Button>
+          {/* Start of Policies Button */}
+          <Flex justify="center" align="center">
+            <Button
+              m={30}
+              height="160px"
+              width="160px"
+              rounded="50%"
+              variant="solid"
+              variantColor="tmarBlack"
+              bg="tmarBlack.800"
+              onClick={() => history.push("/policies")}
+            >
+              <Flex
+                justifyContent="center"
+                alignContent="center"
+                direction="column"
+              >
+                <Box mt={1}>
+                  <Icon name="info" size="3.5rem" color="blue.200" />
+                </Box>
+                <Box>
+                  <Text as="h4" color="white" textTransform="uppercase" mt={4}>
+                    policies
+                  </Text>
+                </Box>
+              </Flex>
+            </Button>
+          </Flex>
+          {/* End of Policies Button */}
+          {/* Start of Settings Button */}
+          <Flex justify="center" align="center">
+            <Button
+              m={30}
+              height="160px"
+              width="160px"
+              rounded="50%"
+              variant="solid"
+              variantColor="tmarBlack"
+              bg="tmarBlack.800"
+              onClick={() => history.push("/settings")}
+            >
+              <Flex
+                justifyContent="center"
+                alignContent="center"
+                direction="column"
+              >
+                <Box mt={1}>
+                  <Icon name="settings" size="3.5rem" color="green.300" />
+                </Box>
+                <Box>
+                  <Text as="h4" color="white" textTransform="uppercase" mt={4}>
+                    settings
+                  </Text>
+                </Box>
+              </Flex>
+            </Button>
+          </Flex>
+          {/* End of Settings Button */}
+          {/* Start of Questions Button */}
+          <Flex justify="center" align="center">
+            <Button
+              m={30}
+              height="160px"
+              width="160px"
+              rounded="50%"
+              variant="solid"
+              variantColor="tmarBlack"
+              bg="tmarBlack.800"
+              onClick={() => history.push("/questions")}
+            >
+              <Flex
+                justifyContent="center"
+                alignContent="center"
+                direction="column"
+              >
+                <Box mt={1}>
+                  <Icon name="question" size="3.5rem" color="yellow.300" />
+                </Box>
+                <Box>
+                  <Text as="h4" color="white" textTransform="uppercase" mt={4}>
+                    questions
+                  </Text>
+                </Box>
+              </Flex>
+            </Button>
+          </Flex>
+          {/* End of Questions Button */}
         </SimpleGrid>
-        <Button
-          m={2}
-          className="CircleBtn"
-          onClick={() => history.push("/alerts")}
-        >
-          <Icon name="bell" size="3.5rem" color="orange.400" />
-          <div className="btn-text">alerts</div>
-        </Button>
+        {/* Start of Alerts Button */}
+        <Flex justify="center" align="center">
+          <Button
+            m={30}
+            height="160px"
+            width="160px"
+            rounded="50%"
+            variant="solid"
+            variantColor="tmarBlack"
+            bg="tmarBlack.800"
+            onClick={() => history.push("/alerts")}
+          >
+            <Flex
+              justifyContent="center"
+              alignContent="center"
+              direction="column"
+            >
+              <Box mt={1}>
+                <Icon name="bell" size="3.5rem" color="orange.400" />
+              </Box>
+              <Box>
+                <Text as="h4" color="white" textTransform="uppercase" mt={4}>
+                  alerts
+                </Text>
+              </Box>
+            </Flex>
+          </Button>
+        </Flex>
+        {/* End of Alerts Button */}
       </Box>
     );
   }
