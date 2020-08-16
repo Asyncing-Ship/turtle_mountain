73154f42e0770cb496c885ff5359cbbfa202ccc0
hunterEdward98 <hunter.scheel@outlook.com>
2020-08-15 19:38:07 -0500

    forgot to put it into app until now

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,18 +4,17 @@ import React from "react";
 // Redux Imports:
 import { connect } from "react-redux";
 // React Router DOM Imports:
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 // Chakra UI Imports:
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 // Custom Chakra theme import:
 import customTheme from "../../style/theme";
 // Components Imports:
-import Navbar from "../Layout/Navbar/Navbar"
+import Navbar from "../Layout/Navbar/Navbar";
 import Content from "../Layout/Content/Content";
 // CSS Import:
 import "./App.css";
+import { ToastComponent } from "../Utilities/ToastComponent/ToastComponent";
 // ----- End of imports -----
 
 export class App extends React.Component {
@@ -27,6 +26,7 @@ export class App extends React.Component {
   render() {
     return (
       <ThemeProvider theme={customTheme}>
+        <ToastComponent />
         <CSSReset />
         <Router>
           <div className="App">
