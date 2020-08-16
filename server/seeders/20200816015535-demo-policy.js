31a8a45246297cc0d4b9b0fe9648f415af707403
Jared Lidenberg <jaredl101@hotmail.com>
2020-08-15 21:16:27 -0500

    Added policy seeder with a bunch of TMAR policies.

---
+++ server/seeders/20200816015535-demo-policy.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("policies", [
+      {
+        filename: "Spay Clinic Application Form.pdf",
+        handle: "Hytph0vtRwSU4GhwJ6dJ!",
+      },
+      {
+        filename: "Fundraising_Event_Request.pdf",
+        handle: "NwyEWprpT3CR9EnruDOp",
+      },
+      {
+        filename: "TMAR_Amazon_Wishlist.pdf",
+        handle: "2hSHBYf2TfyasMnXCIUP",
+      },
+      {
+        filename: "TMAR_Disaster_Application.pdf",
+        handle: "UI0nfBd7SkepLUC0LDGA",
+      },
+      {
+        filename: "TMAR_Adoption_Application_Cat.pdf",
+        handle: "Zhx0W7UtTgigE12jxQjw",
+      },
+      {
+        filename: "TMAR_Foster_Volunteer_Application.pdf",
+        handle: "6kwpKD24TV6Bnr709YLG",
+      },
+      {
+        filename: "TMAR_Adoption_Application_Dog.pdf",
+        handle: "T3NoWffoT1SW4xLa3J5B",
+      },
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("policies", null, {});
+  },
+};
