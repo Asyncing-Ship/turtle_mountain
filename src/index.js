34eb4456d1240fe1f3d20b0f8fb716c449a12aac
Jake Schaffer <jakecr8guru@gmail.com>
2020-08-17 16:46:07 -0500

    added comments to code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
+// ----- Start of imports -----
+// React Import:
 import React from "react";
+// React DOM Imports:
 import ReactDOM from "react-dom";
+// Redux Imports:
 import { createStore, applyMiddleware } from "redux";
+// React Redux Imports:
 import { Provider } from "react-redux";
+// Redux Saga Imports:
 import createSagaMiddleware from "redux-saga";
+// Redux Devtools Extension Imports:
 import { composeWithDevTools } from "redux-devtools-extension";
+// Redux Logger Import:
 import logger from "redux-logger";
-
+// Redux Reducers and Sagas Imports:
 import rootReducer from "./Redux/Reducers"; // imports ./redux/reducers/index.js
 import rootSaga from "./Redux/Sagas"; // imports ./redux/sagas/index.js
-
+// Components Imports:
 import App from "./Components/App/App";
-
+// CSS Import:
 import "./index.css";
+// ----- End of imports -----
 
+// Saga Middleware
 const sagaMiddleware = createSagaMiddleware();
 
 // this line creates an array of all of redux middleware you want to use
@@ -35,6 +45,7 @@ const store = createStore(
 // rootSaga contains all of our other sagas
 sagaMiddleware.run(rootSaga);
 
+// This will render the whole app on build
 ReactDOM.render(
   <Provider store={store}>
     <App />
