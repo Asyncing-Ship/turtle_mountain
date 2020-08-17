c406944bbe6ec4983ad91bcc06702ed55f41551d
Jake Schaffer <jakecr8guru@gmail.com>
2020-08-17 15:29:58 -0500

    added comments to server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
+// Express
 const express = require("express");
+
+// Dotenv
 require("dotenv").config();
+
+// Body Parser
 const bodyParser = require("body-parser");
 
+// Session Middleware
 const sessionMiddleware = require("./modules/session-middleware");
 
+//  ----- These are test requires -----
+// Uncomment code for testing 
 // model imports
 // const User = require("./models/user.model");
 // const Question = require("./models/question.model");
@@ -12,6 +20,7 @@ const sessionMiddleware = require("./modules/session-middleware");
 // const Question_Response = require("./models/question.response.model");
 // const Task_Response = require("./models/task.response.model");
 
+// Variables
 const app = express();
 const passport = require("./strategies/user.strategy");
 require("./strategies/facebook.strategy");
@@ -28,17 +37,19 @@ const questionTagRouter = require("./routes/question.tag.router");
 const taskTagRouter = require("./routes/task.tag.router");
 const notificationRouter = require("./routes/notification.router");
 
+// Middleware 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("server/public"));
 
-// Passport Session Configuration //
+// Passport Session Configuration
 app.use(sessionMiddleware);
 
-// start up passport sessions
+// Start up passport sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
+//  API Paths to routers
 app.use("/api/user", userRouter);
 app.use("/api/auth", oauthRouter);
 app.use("/api/question", questionRouter);
@@ -50,6 +61,7 @@ app.use("/api/question_tag", questionTagRouter);
 app.use("/api/task_tag", taskTagRouter);
 app.use("/api/notification", notificationRouter);
 
+// Server port is listening on:
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
