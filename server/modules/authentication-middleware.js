72b481cfd90bdb247853aa1e44e4d7aeeb010db7
Hunter Scheel <hunter.scheel@outlook.com>
2020-08-24 11:26:54 -0500

    Protecting Mothership, 10% complete

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -9,5 +9,15 @@ const rejectUnauthenticated = (req, res, next) => {
     res.sendStatus(403);
   }
 };
+const rejectUnapproved = (req, res, next) => {
+  if (req.isAuthenticated() && req.user.is_approved) {
+    // They were authenticated! User may do the next thing
+    // Note! They may not be Authorized to do all things
+    next();
+  } else {
+    // failure best handled on the server. do redirect here.
+    res.sendStatus(403);
+  }
+};
 
-module.exports = { rejectUnauthenticated };
+module.exports = { rejectUnauthenticated, rejectUnapproved };
